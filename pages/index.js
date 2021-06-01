230bb1624a75ae236056f5ddaa10f6c22b8b95da
Frail Bongat <frail.bongat@gmail.com>
2021-06-01 17:45:22 +0800

    Style: add global and module styles

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,18 @@
+import Link from 'next/link';
+import styles from '../styles/Home.module.css';
+
 const Home = () => {
   return (
     <div>
-      <h1>Homepage</h1>
-      Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim magnam
-      praesentium iure aut ex corporis dignissimos placeat accusamus quis. Cum.
+      <h1 className={styles.title}>Homepage</h1>
+      <p className={styles.text}>
+        Lorem ipsum dolor sit amet consectetur, adipisicing elit. Enim magnam
+        praesentium iure aut ex corporis dignissimos placeat accusamus quis.
+        Cum.
+      </p>
+      <Link href='/ninjas'>
+        <a className={styles.btn}>See Ninja Listing</a>
+      </Link>
     </div>
   );
 };
