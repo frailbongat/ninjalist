7ca07de16df70899a85898108c9c7faa4fcd5ed8
Frail Bongat <frail.bongat@gmail.com>
2021-06-01 16:52:38 +0800

    Feat: add page setup

---
+++ pages/about.js
@@ -0,0 +1,15 @@
+const About = () => {
+  return (
+    <div>
+      <h1>About</h1>
+      <p>
+        Lorem ipsum dolor sit, amet consectetur adipisicing elit. Dolorem
+        ratione culpa nostrum veritatis quia, voluptate aliquam quaerat
+        accusantium obcaecati quam officiis vero necessitatibus tempore est
+        doloribus deleniti molestiae incidunt? Officia!
+      </p>
+    </div>
+  );
+};
+
+export default About;
